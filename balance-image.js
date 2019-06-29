fdddcd1efaaebb91be568a2f5fcbec919de1f79e
Carlo Denaro <818568+blackout314@users.noreply.github.com>
2019-06-29 15:52:40 +0200

    v0.3

--- balance-image.js
+++ balance-image.js
@@ -7,6 +7,6 @@
 // ==/UserScript==
 
 document.body.innerHTML = document.body.innerHTML.replace( 
-  new RegExp("(([13])[a-zA-HJ-NP-Z0-9]{25,39})","gm"), 
+  new RegExp("([13][a-km-zA-HJ-NP-Z1-9]{25,34}|bc1[ac-hj-np-zAC-HJ-NP-Z02-9]{11,71})","gm"), 
   "$1 <img src='http://btc-priceimg.herokuapp.com/balance/$1/ff0000'/>"
 )
